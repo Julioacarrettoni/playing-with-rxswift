48a760304e59108b35ef67e16bf6c2d52bbcf3ef
Julito <julioacarrettoni@gmail.com>
2020-04-16 00:25:50 -0700

    Updated code to match post 2

--- 003/Before/RxPlaying/Services/Service.swift
+++ 003/Before/RxPlaying/Services/Service.swift
@@ -2,98 +2,28 @@ import FakeService
 import Foundation
 import RxSwift
 
-enum GetSystemStateError: Error {
-    case unknown
-}
-
-protocol ServiceEnpoints {
-    func getSystemState(completion: @escaping (GlobalState?) -> ())
-}
-
-struct Environment {
-    var provider: ServiceEnpoints
-}
-
-extension FakeServices: ServiceEnpoints {}
-
-var Current = RxPlaying.Environment(provider: FakeServices.shared)
-
-struct Service {
-    static func overrideNetworkMock() {
-        self.overrideNetworkMock_V1()
-    }
+final class Service {
+    static var shared = Service()
     
-    static func overrideNetworkMock_V1() {
-        var failures = [false, true]
-        FakeService.Current.failNext = {
-            let next = failures.removeFirst()
-            failures.append(next)
-            return next
-        }
-    }
+    let globalStateUpdates = PublishSubject<GlobalState?>().asObserver()
+    let disposeBag = DisposeBag()
     
-    static func overrideNetworkMock_V2() {
-        var conditions = [
-            (fails: true, delay: 3.0),
-            (fails: true, delay: 3.0),
-            (fails: false, delay: 3.0),
-        ]
+    private init() {
+        let ticksPerSecond = 30
+        let referenceDate = Date.init(timeIntervalSinceNow: 0)
+        FakeService.Current.tick = { -referenceDate.timeIntervalSinceNow * Double(ticksPerSecond)}
+        FakeService.Current.delay = { 0 }
         
-        FakeService.Current.failNext = { conditions.first!.fails }
-        FakeService.Current.delay = {
-            let current = conditions.removeFirst()
-            conditions.append(current)
-            return current.delay
-        }
+        Observable<Int>.interval(.milliseconds(1000/ticksPerSecond), scheduler: MainScheduler.instance)
+            .subscribe(onNext: {[weak self] _ in
+                self?.refreshGlobalState()
+            })
+            .disposed(by: self.disposeBag)
     }
     
-    static func getSystemState(completion: @escaping (GlobalState?) -> () ) {
-        Current.provider.getSystemState(completion: completion)
+    private func refreshGlobalState() {
+        FakeServices.shared.getSystemState { globalState in
+            self.globalStateUpdates.onNext(globalState)
+        }
     }
-    
-    static var systemSingleV1: Single<GlobalState?> = {
-        Single<GlobalState?>.create { single in
-            FakeServices.shared.getSystemState { globalState in
-                single(.success(globalState))
-            }
-            
-            return Disposables.create()
-        }
-    }()
-    
-    static var systemSingle: Single<GlobalState> = {
-        Single<GlobalState>.create { single in
-            FakeServices.shared.getSystemState { globalState in
-                if let globalState = globalState {
-                    single(.success(globalState))
-                } else {
-                    single(.error(GetSystemStateError.unknown))
-                }
-            }
-            
-            return Disposables.create()
-        }
-//        .timeout(.seconds(5), scheduler: MainScheduler.instance)
-        .do(onSuccess: { _ in
-            print("[\(#function)] ✅ Success")
-        }, onError: { error in
-            print("[\(#function)] ❌ request error: \(error)")
-        })
-//            .retry(3)
-    }()
-    
-    static var systemSingle_FINAL: Single<GlobalState> = {
-        Single<GlobalState>.create { single in
-            Current.provider.getSystemState { globalState in
-                if let globalState = globalState {
-                    single(.success(globalState))
-                } else {
-                    single(.error(GetSystemStateError.unknown))
-                }
-            }
-            
-            return Disposables.create()
-        }
-        .retry(3)
-    }()
 }
